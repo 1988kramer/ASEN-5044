25536f007b2a1fe24aeb7065f195af33a8c8dac1
Andrew Kramer <1988kramer@gmail.com>
2018-12-13 21:52:42 -0700

    moved groundtruth generation to separate loop

--- project/EKF.m
+++ project/EKF.m
@@ -1,25 +1,29 @@
 function [P_est,x_est,S_log,ey_log,x_gt] = EKF()
     load("KFdata.mat");
     % best Q so far
+    %{
     Q = [0.55 0 0 0 0 0;
          0 0.77 0 0 0 0;
          0 0 1.70 0 0 0;
          0 0 0 0.85 0 0;
          0 0 0 0 0.92 0;
          0 0 0 0 0 1.66];
+    %}
 
-    %Q = [0.05  0     0    0     0     0;
-    %     0     0.05  0    0     0     0;
-    %     0     0     2.5  0     0     0;
-    %     0     0     0    0.75  0     0;
-    %     0     0     0    0     0.75  0;
-    %     0     0     0    0     0     2.5];
+    Q = [0.05  0     0    0     0     0;
+         0     0.05  0    0     0     0;
+         0     0     2.5  0     0     0;
+         0     0     0    0.75  0     0;
+         0     0     0    0     0.75  0;
+         0     0     0    0     0     2.5];
+    %{
     R = [2.85 0    0    0     0;
          0    41.8 0    0     0;
          0    0    8.15 0     0;
          0    0    0    20.88 0;
          0    0    0    0     17.2944];
-    %R = Rtrue;
+    %}
+    R = Rtrue;
     %Q = zeros(6);
     %Q = 0.25 * Q;
     
@@ -35,45 +39,48 @@ function [P_est,x_est,S_log,ey_log,x_gt] = EKF()
     %P_0 = 1 * eye(6);
     P_p = 1.0 * P_0;        % covariance after update step (P-minus)
     P_m = zeros(6);   % covariance after measurement step (P-plus)
-    P_est = [];       % log of covariance matrices at each timestep
+    P_est = zeros(6,6000);       % log of covariance matrices at each timestep
     
     L = 0.5;          % UGV wheelbase (m)
     deltaT = tvec(2)-tvec(1);
 
     % need to load logged measurements and controls here
     % (or whatever it turns out to be)
-    ey_log = []; % log of measurement errors
-    S_log = [];
+    ey_log = zeros(5,1000); % log of measurement errors
+    S_log = zeros(5,5000);
 
     x_hat_p = x0; % set initial state estimate equal to initial state
                   % may need to actually estimate this later
-    x_est = x_hat_p; % log of estimated system states at each timestep
+    x_est = zeros(6,1001);
+    x_est(:,1) = x_hat_p; % log of estimated system states at each timestep
     
     x_pert = mvnrnd(zeros(1,6),P_0);
-    x_gt = x0 + x_pert'; 
+    
+    x_gt = zeros(6,1001);
+    x_gt(:,1) = x0 + x_pert';
+    
+    u = [2 -pi/18 12 pi/25];
+    
+    % generate groundtruth states
+    for i = 2:1001
+        wk = mvnrnd(zeros(1,6),Qtrue);
+        [~,next_x_gt] = ode45(@motionEqs, [0.0 deltaT], x_gt(:,i-1)', [], u');
+        next_x_gt = next_x_gt(end,:)' + wk';
+        next_x_gt(3) = constrainAngle(next_x_gt(3));
+        next_x_gt(6) = constrainAngle(next_x_gt(6));
+        x_gt(:,i) = next_x_gt;
+    end
 
     for i = 2:1001 %2:size(tvec,2)
 
         % update step
         % calculate new state and covariance from system dynamics
 
-        u = [2 -pi/18 12 pi/25];
-
         % get new state estimate using numerical integration
         [~,ode_x] = ode45(@motionEqs, [0.0 deltaT], x_hat_p', [], u');
         x_hat_m = ode_x(end,:)';
         x_hat_m(3) = constrainAngle(x_hat_m(3));
         x_hat_m(6) = constrainAngle(x_hat_m(6));
-        
-        % get next x groundtruth
-        [~,next_x_gt] = ode45(@motionEqs, [0.0 deltaT], x_gt(:,i-1)', [], u');
-        % add process noise to groundtruth;
-        wk = mvnrnd(zeros(1,6),Qtrue);
-        next_x_gt = next_x_gt(end,:) + wk;
-
-        next_x_gt(3) = constrainAngle(next_x_gt(3));
-        next_x_gt(6) = constrainAngle(next_x_gt(6));
-        x_gt = [x_gt next_x_gt'];
         
         % update estimated state covariance
         % Not using omega matrix because it's I in this case
@@ -84,8 +91,7 @@ function [P_est,x_est,S_log,ey_log,x_gt] = EKF()
 
         % get actual measurement
         vk = mvnrnd(zeros(1,5),Rtrue);
-        y = getMeas(next_x_gt) + vk';
-        %y = getMeas(next_x_gt);
+        y = getMeas(x_gt(:,i)) + vk';
         
         % get predicted measurement using nonlinear model  
         y_hat = getMeas(x_hat_m);
@@ -98,10 +104,10 @@ function [P_est,x_est,S_log,ey_log,x_gt] = EKF()
         
         %lin_ey = y - H*x_hat_m;
             
-        ey_log = [ey_log e_y];
+        ey_log(:,i-1) = e_y;
             
         S = H*P_m*H' + R;
-        S_log = [S_log S];
+        S_log(:,5*(i-2)+1:5*(i-2)+5) = S;
  
         % get the Kalman gain
         % assumes time-invariant R
@@ -114,8 +120,8 @@ function [P_est,x_est,S_log,ey_log,x_gt] = EKF()
         x_hat_p(3) = constrainAngle(x_hat_p(3));
         x_hat_p(6) = constrainAngle(x_hat_p(6));
         
-        x_est = [x_est x_hat_p]; % add newly estimated state to log
-        P_est = [P_est P_p];
+        x_est(:,i) = x_hat_p; % add newly estimated state to log
+        P_est(:,6*(i-2)+1:6*(i-2)+6) = P_p;
 
     end
 end
